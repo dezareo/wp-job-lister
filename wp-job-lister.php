0cae1f950a2ffea7f394c70ccce8a8ee38e725f2
Dragan Zaric <dobrivoje.topalovic@gmail.com>
2021-02-14 17:54:05 +0100

    Update wp-job-lister.php

--- wp-job-lister.php
+++ wp-job-lister.php
@@ -16,11 +16,55 @@ if ( ! defined ( 'ABSPATH' ) ) {
 
 function dez_register_post_type() {
 
-	$args = array( 
+	$singular = 'Job Listing';
+	$plural = 'Job Listings';
 
-		'public' 	=> true,
-		'label'  	=> 'Job Lister',
-		'menu_icon' => 'dashicons-hammer'
+	$labels = array(
+		'name'               => $plural,
+		'singular_name'      => $singular,
+		'add_name'           => 'Add New',
+		'add_new_item'       => 'Add New ' . $singular,
+		'edit'               => 'Edit',
+		'edit_item'          => 'Edit ' . $singular,
+		'new_item'           => 'New ' . $singular,
+		'view'               => 'View ' . $singular,
+		'view_item'          => 'View ' . $singular,
+		'search_term'        => 'Search' . $plural,
+		'parent'             => 'Parent ' . $singular,
+		'not_found'          => 'No ' . $plural . ' found',
+		'not_found_in_trash' => 'No ' . $plural . ' in Trash' 
+	);
+
+	$args = array(
+		'labels'              => $labels,
+		'public'              => true,
+		'publicity_quaryable' => true,
+		'exclude_from_search' => false,
+		'show_in_nav_menus'   => true,
+		'show_ui'             => true,
+		'show_in_menu'        => true,
+		'show_in_admin_bar'   => true,
+		'menu_position'       => 10,
+		'menu_icon'           => 'dashicons-hammer',
+		'can_export'          => true,
+		'delete_with_user'    => false,
+		'hierarchical'        => false,
+		'has_archive'         => true,
+		'query_var'           => true,
+		'capability_type'     => 'post',
+		'map_meta_cap'        => true,
+		'rewrite'             => array(
+			'slug'            => 'jobs',
+			'with_front'      => true,
+			'pages'           => true,
+			'feeds'           => true
+		),
+		'supports' => array(
+			'title',
+			'editor',
+			'author',
+			'custom-fields'
+		)
 	);
 
 	register_post_type( 'job', $args );
