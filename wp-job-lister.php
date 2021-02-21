d6fdb56066fd3a4dc701fb10e7732c95280cffc5
Dragan Zaric <dobrivoje.topalovic@gmail.com>
2021-02-21 23:56:59 +0100

    Enqueue css and js with plugin

--- wp-job-lister.php
+++ wp-job-lister.php
@@ -17,3 +17,14 @@ if ( ! defined ( 'ABSPATH' ) ) {
 require_once ( plugin_dir_path(__FILE__) . 'wp-job-lister-cpt.php' );
 require_once ( plugin_dir_path(__FILE__) . 'wp-job-lister-render-admin.php' );
 require_once ( plugin_dir_path(__FILE__) . 'wp-job-lister-fields.php' );
+
+function dez_admin_enqueue_scripts() {
+   global $pagenow, $typenow;
+
+   if ( $pagenow == 'post.php' || $pagenow == 'post-new.php' && $typenow == 'job' ) {
+   		wp_enqueue_style( 'dez-admin-css', plugins_url( 'css/admin-jobs.css' , __FILE__ ) );
+   		wp_enqueue_script('dez-job-js', plugins_url('js/admin-jobs.js', __FILE__ ), array( 'jquery', 'jquery-ui-datepicker' ), 21022021, true );
+   }
+
+}
+add_action('admin_enqueue_scripts', 'dez_admin_enqueue_scripts');
