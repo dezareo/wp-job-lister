857e09e73be66afa5bad81bbf6da059b9749fb5a
Dragan Zaric <dobrivoje.topalovic@gmail.com>
2021-02-21 21:20:10 +0100

    Update wp-job-lister-fields.php
    Connect input meta fields with database table. Save datas into database.

--- wp-job-lister-fields.php
+++ wp-job-lister-fields.php
@@ -1,4 +1,4 @@
-<?php 
+ <?php 
 
 function dez_add_custom_metabox() {
 
@@ -14,7 +14,9 @@ function dez_add_custom_metabox() {
 }
 add_action('add_meta_boxes', 'dez_add_custom_metabox');
 
-function dez_meta_callback() {
+function dez_meta_callback( $post ) {
+	wp_nonce_field( basename( __FILE__ ), 'dez_jobs_nonce' );
+	$dez_stored_meta = get_post_meta( $post->ID );
 	?>
 
 	<div>
@@ -23,7 +25,23 @@ function dez_meta_callback() {
                 <label for="job-id" class="dez-row-title">Job ID</label>
 			</div>
 			<div class="meta-td">
-				<input type="text" name="job-id" id="job-id" value="" />
+			<input type="text" name="job_id" id="job-id" value="<?php if ( ! empty ($dez_stored_meta['job_id'] ) ) echo esc_attr( $dez_stored_meta['job_id'] [0] ); ?>" />
+			</div>
+		</div>
+		<div class="meta-row">
+			<div class="meta-th">
+                <label for="date-listed" class="dez-row-title">Date Listed</label>
+			</div>
+			<div class="meta-td">
+				<input type="text" name="date_listed" id="date-listed" value="<?php if ( ! empty ($dez_stored_meta['date_listed'] ) ) echo esc_attr( $dez_stored_meta['date_listed'] [0] ); ?>" />
+			</div>
+		</div>
+		<div class="meta-row">
+			<div class="meta-th">
+                <label for="application_deadline" class="dez-row-title">Application Deadline</label>
+			</div>
+			<div class="meta-td">
+				<input type="text" name="application_deadline" id="application-deadline" value="<?php if ( ! empty ($dez_stored_meta['application_deadline'] ) ) echo esc_attr( $dez_stored_meta['application_deadline'] [0] ); ?>" />
 			</div>
 		</div>
 	</div>
@@ -46,3 +64,29 @@ function dez_meta_callback() {
 <?php
 
 }
+
+function dez_meta_save( $post_id ) {
+	//Checks save status
+	$is_autosave = wp_is_post_autosave( $post_id );
+	$is_revision = wp_is_post_revision( $post_id );
+	$is_valid_nonce = ( isset ( $_POST[ 'dez_jobs_nonce' ] ) && wp_verify_nonce( $_POST ['dez_jobs_nonce'], basename( __FILE__ ) ) ) ? 'true' : 'false';
+
+	//Exits script depending on save status
+	if ( $is_autosave || $is_revision || !$is_valid_nonce ) {
+		return;
+	}
+
+	if ( isset ( $_POST['job_id'] ) ) {
+		update_post_meta( $post_id, 'job_id', sanitize_text_field(  $_POST[ 'job_id' ] ) );
+	}
+	if ( isset ( $_POST['date_listed'] ) ) {
+		update_post_meta( $post_id, 'date_listed', sanitize_text_field(  $_POST[ 'date_listed' ] ) );
+	}
+	if ( isset ( $_POST['application_deadline'] ) ) {
+		update_post_meta( $post_id, 'application_deadline', sanitize_text_field(  $_POST[ 'application_deadline' ] ) );
+	}
+	if ( isset ( $_POST['principle_duties'] ) ) {
+		update_post_meta( $post_id, 'principle_duties', sanitize_text_field(  $_POST[ 'principle_duties' ] ) );
+	}
+}
+add_action( 'save_post', 'dez_meta_save' );
