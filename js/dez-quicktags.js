498b99ef3e186f72ed185bf4d66073efabeae247
Dragan Zaric <dobrivoje.topalovic@gmail.com>
2021-02-22 22:34:55 +0100

    Create dez-quicktags.js

---
+++ js/dez-quicktags.js
@@ -0,0 +1,11 @@
+//jQuery
+jQuery(document).ready(function(){
+	quicktags({
+		id: 'minimum-requirements',
+		buttons: 'em,strong,link'
+	});
+	quicktags({
+		id: 'prefered-requirements',
+		buttons: 'em,strong,link'
+	});
+});
