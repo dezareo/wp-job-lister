631bb9e8204ad3e673b0123ad4b0614ab82edf67
Dragan Zaric <dobrivoje.topalovic@gmail.com>
2021-02-28 18:55:38 +0100

    Add WP_Query object to plugin

--- wp-job-lister-render-admin.php
+++ wp-job-lister-render-admin.php
@@ -1,5 +1,19 @@
 <?php
 
 function reorder_admin_jobs_callback() {
-	echo 'This is the jobs reorder admin page';
+	
+	$args = array(
+		'post_type' 			=> 'job',
+		'orderby' 				=> 'menu_order',
+		'order' 				=> 'ASC',
+		'no_found_rows' 		=> true,
+		'update_post_term_cache' => false,
+		'post_per_post' 		=> 50
+	);
+
+	$job_listing = new WP_Query( $args );
+	echo '<pre>';
+	var_dump( $job_listing );
+	echo '</pre>';
+
 }
