019332adb0904c33d8c6b6dee89c1bb406bec07c
Dragan Zaric <dobrivoje.topalovic@gmail.com>
2021-03-02 23:16:31 +0100

    Update reorder.js

--- js/reorder.js
+++ js/reorder.js
@@ -1,8 +1,30 @@
 jQuery(document).ready(function($){
 
 	var sortList = $( 'ul#custom-type-list' );
+	var animation = $( '#loading-animation' );
+	var pageTitle = $( 'div h2');
 
 	sortList.sortable({
+
+		update: function( event, ui ) {
+			animation.show();
+
+			$.ajax({
+				url: ajaxurl,
+				type: 'POST',
+				dataType: 'json',
+				data: {
+					action: 'save_post',
+					order: sortList.sortable('toArray').toString()
+				},
+				success: function ( response ) {
+					console.log('success');
+				},
+				error: function ( error ) {
+					console.log('error');
+				}
+			});
+		}
 
 	});
 
